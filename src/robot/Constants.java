c66540342ab9678c2e3d4a01e0d934b892892772
gregoryhazen <Gregoryhazen@gmail.com>
2018-01-30 17:41:09 -0500

    1/30 Stuff
    Initial setup.  Didn't get to test yet.

---
+++ src/robot/Constants.java
@@ -0,0 +1,7 @@
+package robot;
+
+public class Constants {
+    public static final int PORT_XBOX_DRIVE             =3;
+    public static final int PORT_INTAKE_LEFT            =4;
+    public static final int PORT_INTAKE_RIGHT           =5;
+}
--- src/robot/Robot.java
+++ src/robot/Robot.java
@@ -1,16 +1,32 @@
 package robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
+
+    public double intakeSpeed;
+    public XboxController xboxDrive;
+
+    public static TalonSRX intakeLeft = new TalonSRX(Constants.PORT_INTAKE_LEFT);
+    public static TalonSRX intakeRight = new TalonSRX(Constants.PORT_INTAKE_RIGHT);
+
     @Override
-    public void robotInit() { }
+    public void robotInit() {
+        xboxDrive = new XboxController(Constants.PORT_XBOX_DRIVE);
+        intakeSpeed = 0.0;
+        SmartDashboard.putNumber("Intake Speed", 0.0);
+        intakeRight.follow(intakeLeft);
+
+    }
 
     @Override
     public void disabledInit() { }
 
-    @Override
-    public void autonomousInit() { }
 
     @Override
     public void teleopInit() { }
@@ -21,12 +37,23 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void disabledPeriodic() { }
-    
-    @Override
-    public void autonomousPeriodic() { }
+
 
     @Override
-    public void teleopPeriodic() { }
+    public void teleopPeriodic() {
+
+        //Smart Dashboard
+
+        intakeSpeed = SmartDashboard.getNumber("Intake Speed", 0.0);
+
+
+        //Remote
+
+        //intakeSpeed = xboxDrive.getTriggerAxis(XboxController.Hand.kRight);
+
+        intakeLeft.set(ControlMode.PercentOutput, intakeSpeed);
+
+    }
 
     @Override
     public void testPeriodic() { }
